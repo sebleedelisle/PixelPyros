a182f9faeccd9e88d342b7a06cef12543b9d3eeb
Paul Hayes <paul.hayes@entropedia.co.uk>
2013-08-24 01:19:47 +0100

    Added additional gui elements to test fb mode for guiGroup

--- BetterGUI/src/ofApp.h
+++ BetterGUI/src/ofApp.h
@@ -4,26 +4,29 @@
 #include "ofxGui.h"
 
 class ofApp : public ofBaseApp{
-
-	public:
     
-        ofxPanel gui;
-        ofxToggle toggleDemo;
-
+public:
     
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
-
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    ofxPanel gui;
+    ofxToggle toggleDemo;
+    ofxSlider<float> sliderDemo;
+    ofxSlider<int> intSliderDemo;
+    ofxColorSlider colorSliderDemo;
+    ofxButton buttonDemo;
+    
+    
 };
