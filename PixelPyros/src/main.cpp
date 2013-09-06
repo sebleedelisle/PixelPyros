465a0e1b3da85ace354fef70d011c9aa6960bb1f
Paul Hayes <paul.hayes@entropedia.co.uk>
2013-09-06 15:04:07 +0100

    Changing PixelPyros app class back to normal

--- PixelPyros/src/main.cpp
+++ PixelPyros/src/main.cpp
@@ -1,14 +1,11 @@
 #include "ofMain.h"
 #include "ofApp.h"
-#include "ofxFensterManager.h"
-//#include "ofAppGlutWindow.h"
+#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
 
-
-    ofxFensterManager::setup(1536,1024,OF_WINDOW);
-	//ofSetupOpenGL(1536,1024, OF_WINDOW);			// <--------
+    ofSetupOpenGL(1536,1024, OF_WINDOW);			// <--------
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
