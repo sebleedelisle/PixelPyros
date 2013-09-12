7024ae81476cc2d0a70603a785d14855e7052ae7
Paul Hayes <paul.hayes@entropedia.co.uk>
2013-09-12 11:16:30 +0100

    Enabled fullscreen spanning, added Parameter Manager to project

--- PixelPyros/src/main.cpp
+++ PixelPyros/src/main.cpp
@@ -1,15 +1,18 @@
 #include "ofMain.h"
 #include "ofApp.h"
-#include "ofAppGlutWindow.h"
+#include "ofAppGLFWWindow.h"
 
 //========================================================================
 int main( ){
 
+    ofAppGLFWWindow* window = new ofAppGLFWWindow();
+    window->setMultiDisplayFullscreen(true);
 
-	ofSetupOpenGL(1536,1024, OF_WINDOW);			// <--------
+	ofSetupOpenGL(window,1536,1024, OF_WINDOW);			// <--------
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp( new ofApp());
+    
 
 }
