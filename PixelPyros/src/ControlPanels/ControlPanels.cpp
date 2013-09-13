d81ae655f7da93d029a2e04fcfde9aad863623dc
Paul Hayes <paul.hayes@entropedia.co.uk>
2013-09-13 16:08:08 +0100

    Moved motion gui

--- PixelPyros/src/ControlPanels/ControlPanels.cpp
+++ PixelPyros/src/ControlPanels/ControlPanels.cpp
@@ -17,23 +17,23 @@ void ControlPanels::setup(ParameterManager * parameterManager){
 	ofxBaseGui::setDefaultElementIndentation(1);
 	ofxBaseGui::setDefaultTextPadding(7);
     
-    float offsetX = 160;
+    ofVec2f offset(160,40);
     
-    setupPanel( "Laser", "laserSettings.xml", ofRectangle( offsetX, 0, 400, 30 ), laserGui );
+    setupPanel( "Laser", "laserSettings.xml", ofRectangle( offset.x, offset.y, 400, 30 ), laserGui );
     laserGui.add( *parameterManager->getParameterGroup("laser") );
     laserGui.load();
     laserGui.getGroup("Laser Manager").getToggle("Etherdream connect") = false;
     laserGui.getGroup("Laser Manager").getLabel("status") = "";
     
-    setupPanel( "Renderer", "rendererSettings.xml", ofRectangle( offsetX + 420, 0, 400, 30 ), rendererGui );
+    setupPanel( "Renderer", "rendererSettings.xml", ofRectangle( offset.x + 420, offset.y, 400, 30 ), rendererGui );
     rendererGui.add( *parameterManager->getParameterGroup("renderer") );
     rendererGui.load();
     
-    setupPanel( "Triggers", "triggerSettings.xml", ofRectangle( offsetX + 840, 0, 400, 30 ), triggerGui );
+    setupPanel( "Triggers", "triggerSettings.xml", ofRectangle( offset.x + 840, offset.y, 400, 30 ), triggerGui );
     triggerGui.add( *parameterManager->getParameterGroup("triggers") );
     triggerGui.load();
     
-    setupPanel( "Motion", "motionDetectionSettings.xml", ofRectangle( offsetX + 1260, 0, 400, 30 ), motionGui );
+    setupPanel( "Motion", "motionDetectionSettings.xml", ofRectangle( offset.x, offset.y * 2 + laserGui.getHeight(), 400, 30 ), motionGui );
     motionGui.add( *parameterManager->getParameterGroup("motion") );
     motionGui.load();
 
