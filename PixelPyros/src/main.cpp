3060e9f03b0625c75fa10171343042954e4c4565
Paul Hayes <paul.hayes@entropedia.co.uk>
2013-09-16 11:16:32 +0100

    Changed to fullscreen by default, Positioned ui based on the x position of the furthest right monitor

--- PixelPyros/src/main.cpp
+++ PixelPyros/src/main.cpp
@@ -8,11 +8,10 @@ int main( ){
     ofAppGLFWWindow* window = new ofAppGLFWWindow();
     window->setMultiDisplayFullscreen(true);
 
-	ofSetupOpenGL(window,1536,1024, OF_WINDOW);			// <--------
+	ofSetupOpenGL(window,1536,1024, OF_FULLSCREEN);			// <--------
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp( new ofApp());
-    
 
 }
